aa1ce16c71cbd192899081f86e4534a8d8dd2051
Radvil <radvil.linux@gmail.com>
2024-03-29 23:20:39 +0800

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'neo-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  template: `
+    <h1>Welcome to {{title}}!</h1>
+    <router-outlet />
+  `,
+})
+export class AppComponent {
+  title = 'neong';
+}
