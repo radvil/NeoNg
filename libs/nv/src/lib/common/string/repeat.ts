a05c28bd84535eb09db93f51634f64867c1d35a2
Radvil <radvil.linux@gmail.com>
2024-03-30 02:58:19 +0800

    feat(common): revert string repeat

--- libs/nv/src/lib/common/string/repeat.ts
+++ libs/nv/src/lib/common/string/repeat.ts
@@ -1,4 +1,4 @@
-import { hasUnicode } from "./has-unicode";
+import { stringSize } from "./string-size";
 
 /**
  * Repeats the given string `n` times.
@@ -11,31 +11,22 @@ import { hasUnicode } from "./has-unicode";
  *
  * repeat('abc', 0) // ''
  */
-export const repeat = (value: string, n: number): string => {
+export const repeat = (value: string, n: number, separator?: string): string => {
+  const withSeparator = typeof separator === "string";
   let result = "";
-  if (hasUnicode(value)) {
-    for (let i = 0; i < n; i++) {
-      result += value
-    }
-    return result
+  if (withSeparator) value = value + separator;
+  if (!(!value || n < 1 || n > Number.MAX_SAFE_INTEGER)) {
+    // Leverage the exponentiation by squaring algorithm for a faster repeat.
+    // See https://en.wikipedia.org/wiki/Exponentiation_by_squaring for more details.
+    do {
+      if (n % 2) {
+        result += value;
+      }
+      n = Math.floor(n / 2);
+      if (n) {
+        value += value;
+      }
+    } while (n);
   }
-  try {
-    if (!value || n < 1 || n > Number.MAX_SAFE_INTEGER) {
-      return result;
-    }
-  } catch (e) {
-    return result;
-  }
-  // Leverage the exponentiation by squaring algorithm for a faster repeat.
-  // See https://en.wikipedia.org/wiki/Exponentiation_by_squaring for more details.
-  do {
-    if (n % 2) {
-      result += value;
-    }
-    n = Math.floor(n / 2);
-    if (n) {
-      value += value;
-    }
-  } while (n);
-  return result;
+  return withSeparator ? result.slice(0, -stringSize(separator)) : result;
 };
