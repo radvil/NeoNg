9c7be7bc34b1583e300cc36a59d5370e8a68f3a5
Radvil <radvil.linux@gmail.com>
2024-04-20 09:26:02 +0700

    feat(playground): Update playground

--- src/app/@pages/playground/playground.cmp.ts
+++ src/app/@pages/playground/playground.cmp.ts
@@ -1,5 +1,43 @@
-import { ChangeDetectionStrategy, Component, computed, signal } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  signal,
+} from "@angular/core";
 import { it, v } from "@neo/utils";
+import { filter, map, range, toArray } from "rxjs";
+import { JsonPipe } from "@angular/common";
+// import * as it from "lazy-collections/src/index";
+
+/*
+ * If you’re dealing with class methods,
+ * you can still use the .name property, but keep in mind that
+ * minification and bundling tools might change function names,
+ * so it’s not always reliable in a production environment.
+ * For more complex scenarios, such as getting the name of the
+ * current method from within an instance method of a class,
+ * you might need to use decorators or other advanced TypeScript features1.
+ */
+function AnnotateName(severity: "log" | "warn" | "error" = "log") {
+  return function (
+    _: any,
+    name: string | symbol,
+    descriptor: PropertyDescriptor,
+  ) {
+    const originalMethod = descriptor.value;
+    descriptor.value = function (...args: any[]) {
+      console[severity](`Calling Method: ${String(name)}`);
+      return originalMethod.apply(this, args);
+    };
+  };
+}
+
+const V = {
+  FilterMin: 100,
+  FilterMax: 100_000,
+  RangeMin: 0,
+  RangeMax: 1_000_000_000,
+} as const;
 
 @Component({
   standalone: true,
@@ -7,6 +45,7 @@ import { it, v } from "@neo/utils";
   styleUrl: "playground.cmp.scss",
   templateUrl: "playground.cmp.html",
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [JsonPipe],
 })
 export class PlaygroundCmp {
   readonly name = signal("injoker 🎲 madafaka");
@@ -20,19 +59,69 @@ export class PlaygroundCmp {
   readonly nameTitleCased = computed(() => v.titlecase(this.name()));
   readonly name3x = computed(() => v.repeat(this.name(), 3, " | "));
 
-  readonly collectPrimes = it.pipe(
-    it.filter((x: number) => x < 10_000),
+  readonly collectNumbers = it.pipe(
+    it.filter((x: number) => x < V.FilterMax),
     it.map((x) => ({ value: x })),
-    it.filter((x) => x.value > 1_000),
+    it.filter((x) => x.value > V.FilterMin),
     it.toArray(),
   );
 
-  readonly totalPrimNumbers = signal(0);
+  readonly rxjs$ = range(V.RangeMin, V.RangeMax).pipe(
+    filter((x) => x < V.FilterMax),
+    map((x) => ({ value: x })),
+    filter((x) => x.value > V.FilterMin),
+    toArray(),
+    map((x) => x.length),
+  );
 
-  gen(): void {
-    const results = this.collectPrimes(it.primesInRange(0, 1_000_000));
-    this.totalPrimNumbers.set(results.length);
-    console.warn(results);
+  readonly logs = signal<{ [id: string]: number }>({});
+
+  measure(fn: Function): void {
+    const start = performance.now();
+    fn();
+    const finish = performance.now();
+    console.warn(`Finished in ${finish - start}`);
+  }
+
+  @AnnotateName()
+  reactiveX(): void {
+    this.measure(() => {
+      this.rxjs$.subscribe((total) => {
+        this.logs.update((x) => {
+          x["reactiveX"] = total;
+          return x;
+        });
+      });
+    });
+    console.warn(this.logs());
+  }
+
+  @AnnotateName()
+  lazy(): void {
+    this.measure(() => {
+      this.logs.update((x) => {
+        x["lazy"] = this.collectNumbers(
+          it.range(V.RangeMin, V.RangeMax),
+        ).length;
+        return x;
+      });
+    });
+    console.warn(this.logs());
+  }
+
+  @AnnotateName()
+  eager(): void {
+    this.measure(() => {
+      this.logs.update((x) => {
+        x["eager"] = v
+          .countWithinRange(V.RangeMin, V.RangeMax)
+          .filter((x) => x < V.FilterMax)
+          .map((x) => ({ value: x }))
+          .filter((x) => x.value > V.FilterMin).length;
+        return x;
+      });
+    });
+    console.warn(this.logs());
   }
 
   ngOnInit(): void {}
